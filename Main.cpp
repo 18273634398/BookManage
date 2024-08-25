06b6e22e1a89fe7ad61359f7d748721c41f25ddf
18273634398 <70963841+18273634398@users.noreply.github.com>
2024-08-25 11:43:04 +0800

    更新 Main.cpp
    chat4o修改意见

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 // B+树的阶数
-const int M = 5;
+const int M = 3;  // 示例中设置较小的M值，方便调试
 
 // B+树节点类型
 enum NodeType {
     INTERNAL,  // 内部节点
-    LEAF  // 叶子节点
+    LEAF       // 叶子节点
 };
 
 // B+树节点结构
@@ -19,9 +19,9 @@ struct Node {
     vector<int> keys;
     vector<Node*> children;
     Node* parent;
-    bool is_nul;
+    Node* next;  // 用于叶子节点之间的链接
 
-    Node(NodeType t, bool n) : type(t), is_nul(n) {}
+    Node(NodeType t) : type(t), parent(nullptr), next(nullptr) {}
 };
 
 class BPlusTree {
@@ -31,14 +31,14 @@ private:
 
 public:
     BPlusTree() {
-        root = new Node(INTERNAL, true);
+        root = new Node(LEAF);  // 初始时树为空，根节点为叶子节点
         height = 0;
     }
 
     // 插入键值对
     void insert(int key) {
         if (root->keys.size() == (2 * M - 1)) {
-            Node* new_root = new Node(INTERNAL, false);
+            Node* new_root = new Node(INTERNAL);
             new_root->children.push_back(root);
             split_child(new_root, 0);
             root = new_root;
@@ -61,13 +61,26 @@ private:
     // 分裂子节点
     void split_child(Node* parent, int index) {
         Node* child = parent->children[index];
-        Node* new_child = new Node(child->type, false);
+        Node* new_child = new Node(child->type);
+        
+        // 分割键值
         parent->keys.insert(parent->keys.begin() + index, child->keys[M - 1]);
         parent->children.insert(parent->children.begin() + index + 1, new_child);
-        new_child->keys.assign(child->keys.begin() + M, child->keys.end());
-        new_child->children.assign(child->children.begin() + M, child->children.end());
-        child->keys.resize(M - 1);
-        child->children.resize(M);
+        
+        // 处理叶子节点
+        if (child->type == LEAF) {
+            new_child->keys.assign(child->keys.begin() + M - 1, child->keys.end());
+            child->keys.resize(M - 1);
+            
+            // 更新叶子节点的链接
+            new_child->next = child->next;
+            child->next = new_child;
+        } else {  // 处理内部节点
+            new_child->keys.assign(child->keys.begin() + M, child->keys.end());
+            new_child->children.assign(child->children.begin() + M, child->children.end());
+            child->keys.resize(M - 1);
+            child->children.resize(M);
+        }
     }
 
     // 在非满节点中插入键值对
@@ -111,17 +124,15 @@ int main() {
     BPlusTree bpt;
     bpt.insert(10);
     bpt.insert(20);
+    bpt.insert(5);
+    bpt.insert(6);
+    bpt.insert(12);
     bpt.insert(30);
-    bpt.insert(40);
-    bpt.insert(50);
-    bpt.insert(60);
-    bpt.insert(70);
-    bpt.insert(80);
-    bpt.insert(90);
-    bpt.insert(100);
+    bpt.insert(7);
+    bpt.insert(17);
 
-    cout << "Search for 50: " << (bpt.search(50) ? "Found" : "Not found") << endl;
-    cout << "Search for 110: " << (bpt.search(110) ? "Found" : "Not found") << endl;
+    cout << "Search for 6: " << (bpt.search(6) ? "Found" : "Not found") << endl;
+    cout << "Search for 15: " << (bpt.search(15) ? "Found" : "Not found") << endl;
 
     return 0;
 }
